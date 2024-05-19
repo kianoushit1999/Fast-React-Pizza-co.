a1fb77533b3d4cbb87c9602e9adc194b388be92b
kianoushit1999 <kianoushit1999@gmail.com>
2024-05-19 20:49:39 +0330

    create seperate file for App

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App  from "./App";
 
-function App() {
-    return <h1> Hello World </h1>
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<React.StrictMode>
