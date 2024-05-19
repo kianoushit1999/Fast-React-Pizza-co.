a6ce0b59d1a8a72d374fa17b4b74921ec22a75a3
kianoushit1999 <kianoushit1999@gmail.com>
2024-05-19 20:53:50 +0330

    header component was added to the App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
+import Header from "Header";
+
 function App() {
-    return <h1> Hello World </h1>
+    return <div>
+        <Header />
+    </div>
 }
 
 export default App;
